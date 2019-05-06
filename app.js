918e2ace526d450d44b2ae4e36356b65e965e60f
jwkeena <jwkeena@gmail.com>
2019-05-06 15:03:33 -0400

    Added stored user name functionality

--- app.js
+++ app.js
@@ -1,16 +1,79 @@
-  // Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyCQcBuXgvKhbiNs4a2FTO3fEzhd_4G7J9M",
-    authDomain: "multiplayer-fa27c.firebaseapp.com",
-    databaseURL: "https://multiplayer-fa27c.firebaseio.com",
-    projectId: "multiplayer-fa27c",
-    storageBucket: "multiplayer-fa27c.appspot.com",
-    messagingSenderId: "89411635735",
-    appId: "1:89411635735:web:757a31e9fce84ddf"
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCmhH-MHk71LAzeyzO5dlvTbhdNVAol1Yw",
+    authDomain: "fir-test-cfa84.firebaseapp.com",
+    databaseURL: "https://fir-test-cfa84.firebaseio.com",
+    projectId: "fir-test-cfa84",
+    storageBucket: "fir-test-cfa84.appspot.com",
+    messagingSenderId: "694044494784"
   };
 
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  // Shorthand firebase variable
-  const database = firebase.database();
+// Create shorthand firebase variable
+const database = firebase.database();
+
+// Initialize global variables
+let player1Name = "";
+let player2Name = "";
+
+// Game functions
+const game = {
+    updatePlayerName: function (spanID, newName) {
+        $(spanID).text(newName)
+    }
+}
+
+// Creates users node if it doesn't already exist on pageload
+database.ref().once("value", function (snapshot) {
+    
+    // Grab and display stored player names
+    if (snapshot.child("users").exists()) {
+        
+        player1NameStored = snapshot.val().users.player1;
+        player2NameStored = snapshot.val().users.player2;
+
+        $("#name1").text(player1NameStored)
+        $("#name2").text(player2NameStored)
+
+        return;
+    // If there is no users node, create placeholder names
+    } else {
+        database.ref().set( {
+            users: {
+                player1: "Player 1",
+                player2: "Player 2",
+            }
+        })
+    }
+})
+
+
+$("#name-set1").on("click", function () {
+    event.preventDefault();
+    newNamePlayer1 = $("#nameChoice1").val();
+    database.ref().child("users").update(
+        {player1: newNamePlayer1}, 
+    );
+    game.updatePlayerName("#name1", newNamePlayer1);
+})
+
+$("#name-set2").on("click", function () {
+    event.preventDefault();
+    newNamePlayer2 = $("#nameChoice2").val();
+    database.ref().child("users").update(
+        {player2: newNamePlayer2}, 
+    );
+    game.updatePlayerName("#name2", newNamePlayer2);
+})
+
+
+database.ref().on("child_added", function (snapshot) {
+    let snap = snapshot.val();
+    $("#firebase-log").prepend(snap.chat)
+
+    // Handle the errors
+    }, function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+  });
